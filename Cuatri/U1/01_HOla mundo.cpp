398f337e653f71c764bcc501990365b3132207e2
Paulina <up210374@alumnos.upa.edu.mx>
2022-09-12 10:49:54 -0500

    hola mundo

---
+++ Cuatri/U1/01_HOla mundo.cpp
@@ -0,0 +1,20 @@
+/*
+Date: 12/09/2022
+Author: Paulina Jaquelin Álvarez Martínez
+Email: up210374@alumnos.upa.edu.mx
+Description: Mi primer programa en c++
+*/
+
+//---------PREPROCESSOR DIRECTIVES--------
+
+//Input/output library between the screen n' keyboard
+#include <iostream>
+
+//-------- MAIN FUNCTION -------------
+int main (){
+    
+    //Prints on screen a text
+    std::cout<< "Hello Cruel world, this is my first program";
+    //Return 0 that the program ends succesfully
+    return 0;
+}
