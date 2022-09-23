edf18c7967a3f204c52d4b79142fc14db4079ea3
Paulina <up210374@alumnos.upa.edu.mx>
2022-09-23 10:01:55 -0500

    comentarios

--- Cuatri/U2/01_Renta.cpp
+++ Cuatri/U2/01_Renta.cpp
@@ -9,10 +9,13 @@
 using namespace std;
 
 int main (){
+        //Declaration of variables
     int income;
     float income1, totalAmountdue,tax, tax1;
-    cout << "Enter your annual income: $";
+        //Ask the user your income
+    cout << "Enter your annual income: $"; 
     cin >> income;
+        //Assess the user's income to obtain the percentage to be paid
     if (income>0 && income<10000){
         tax=0.05;
     }
@@ -28,10 +31,12 @@ int main (){
     else if (income>60000){
             tax=0.45;
     }
+        //Calculate total amount due the user with your tax
     tax1= tax*100;
     cout << "Your tax is: " << tax1 << "%" << endl;
     income1=(income*tax);
     totalAmountdue=(income-income1);
+        //Show the user your total amount due
     cout << "Your total amount due is: " << "$" << totalAmountdue << endl;
     return 0;
 }
