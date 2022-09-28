a141132e3ca6765bbf735d4c7effb64e8df779dd
Paulina <up210374@alumnos.upa.edu.mx>
2022-09-28 09:57:08 -0500

    ejercicio3

---
+++ Cuatri/U2/04_Pizzas.cpp
@@ -0,0 +1,7 @@
+/*
+    Unit 2. ConUnit 2. Conditionals
+    Author: Paulina Jaquelin Alvarez Martinez
+    Date: 28/09/2022
+    Description: Shows the amount to be paid to enter the gaming room according to your age. 
+
+*/
