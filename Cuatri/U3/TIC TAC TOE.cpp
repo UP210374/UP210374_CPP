dee2a2a78ef3075dd2ccada104634a66282e45d0
Paulina <up210374@alumnos.upa.edu.mx>
2022-11-16 20:26:53 -0600

    update

--- Cuatri/U3/TIC TAC TOE.cpp
+++ Cuatri/U3/TIC TAC TOE.cpp
@@ -1,150 +1,268 @@
 /*
-Author: Paulina Jaquelin Alvarez Martinez 
+Author: Paulina Jaquelin Alvarez Martinez
 Date: 12/11/2022
-Description: Show the tic tac toe game 
+Description: Show the tic tac toe game
 */
 
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
-void construirTablero(char areaPartida[3][3]);
-char areaPartida[3][3]={{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-int seleccionarPartida();
-bool comprobarCasillaOcupada(int);
-int colocarPartida(int partida);
-bool esGanador(char areaPartida);
-int Ganador;
-int row, col, turnoJugador;
+void displayBoard();
+void move();
+int selectPlay();
+bool checkBox(int);
+int setPlay(int);
+bool isWinner();
 
-int main (){
-    int partida;
-    bool casillaOcupada=true;
-    cout << "Bienvenido al juego TIC-TAC-TOE" << endl << endl;
-    construirTablero(areaPartida);
-    do{
-        partida=seleccionarPartida();
-        casillaOcupada=comprobarCasillaOcupada(partida);
-        if (casillaOcupada==true){
-            cout << "Selecciona otra partida";
-        }else {
-            colocarPartida(partida);
-            esGanador(Ganador);
+int winner, play;
+int row, col, turn;
+char position[6][11] = {{' '}};
+
+int main()
+{
+    char option;
+    bool winner = false;
+    cout << "TIC-TAC-TOE GAME :D" << endl
+         << endl;
+    cout << "Press Y to play or X to exit the game:  " << endl;
+    cin >> option;
+    if (option == 'Y' || option == 'y')
+    {
+        bool occupiedSquare = true;
+        cout << "Even-numbered items are o and odd-numbered items are x" << endl
+             << endl;
+        move();
+        displayBoard();
+        cout << endl;
+        do
+        {
+            play = selectPlay();
+            occupiedSquare = checkBox(play);
+            if (occupiedSquare == true)
+            {
+                cout << "Select other play";
+            }
+            else
+            {
+                setPlay(play);
+                system("clear");
+                displayBoard();
+                cout << endl;
+                winner = isWinner();
+            }
+            turn++;
+        } while (turn <= 9 || winner == true);
+        if (winner == true)
+        {
+            cout << "Congratulations you have won absolutely nothing but my appreciation for playing this game :)";
         }
-        }while (casillaOcupada==true);
+        else
+        {
+            cout << "oh sorry, you lost :(";
+        }
+        
+    }
+    else
+    {
+        cout << "I didn't even want you to play :D" << endl
+             << endl;
+    }
+
     return 0;
 }
-
-void construirTablero (char areaPartida[3][3]){
-    cout << endl;   
-    cout << "--|---|--";
-    cout << endl;
-    for (int row=0;row<3;row++){
-        for (int col=0;col<3;col++){
-            if (row==0 && col==1 || row==1 && col==1 || row==2 && col==1 ){
-                cout << " | " << areaPartida[row][col]<< " | ";
-            }else {
-                 cout << areaPartida[row][col]; 
+void displayBoard()
+{
+    int board[6][11];
+    for (int row = 0; row < 6; row++)
+    {
+        for (int col = 0; col < 11; col++)
+        {
+            if (row != 1 && row != 3 && col != 3 && col != 7)
+            {
+                if ((row == 0 || row == 2 || row == 4) && (col == 1 || col == 5 || col == 9))
+                {
+                    cout << position[row][col];
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            else if (col == 3 || col == 7)
+            {
+                cout << "|";
+            }
+            else if (row == 1 || row == 3)
+            {
+                cout << "_";
             }
         }
         cout << endl;
-        cout << "--|---|--";
-    cout << endl;
     }
 }
 
-
-int seleccionarPartida(){
-    int partida;
-    do{
-        cout << "Selecciona tu partida: ";
-        cin >> partida;
-    }while (partida<=9&&partida>0);
-    return partida;
+void move()
+{
+    position[0][1] = '1';
+    position[0][5] = '2';
+    position[0][9] = '3';
+    position[2][1] = '4';
+    position[2][5] = '5';
+    position[2][9] = '6';
+    position[4][1] = '7';
+    position[4][5] = '8';
+    position[4][9] = '9';
 }
 
-bool comprobarCasillaOcupada(int partida){
-    if (partida==1){
-        row=0;
-        col=0;
-    }else if (partida==2){
-        row=0;
-        col=1;
-    }else if (partida==3){
-        row=0;
-        col=2;
-    }else if (partida==4){
-        row=1;
-        col=0;
-    }else if (partida==5){
-        row=1;
-        col=1;
-    }else if (partida==6){
-        row=1;
-        col=2;
-    }else if (partida==7){
-        row=2;
-        col=0;
-    }else if (partida==8){
-        row=2;
-        col=1;
-    }else if (partida==9){
-        row=2;
-        col=2;
-    }
+int selectPlay()
+{
+    cout << "Player"
+         << " " << turn % 2 + 1 << endl;
+    cout << "Select your move: ";
+    cin >> play;
+    return play;
+}
 
-    if (areaPartida[row][col]=='x'|| areaPartida[row][col]=='o'){
+bool checkBox(int play)
+{
+    if (position[row][col] == 'x' || position[row][col] == 'o')
+    {
         return true;
-    }else 
-    return false;
+    }
+    else
+    {
+        return false;
+    }
 }
 
-int colocarPartida(int partida){
-    int colocarPartida;
-     colocarPartida=(turnoJugador%2==0)?'x':'o';
-     return colocarPartida;
+int setPlay(int play)
+{
+    int value;
+    if (turn % 2 == 1)
+    {
+        value = 'x';
+    }
+    else
+    {
+        value = 'o';
+    }
+    if (play == 1)
+    {
+        position[0][1] = value;
+    }
+    else if (play == 2)
+    {
+        position[0][5] = value;
+    }
+    else if (play == 3)
+    {
+        position[0][9] = value;
+    }
+    else if (play == 4)
+    {
+        position[2][1] = value;
+    }
+    else if (play == 5)
+    {
+        position[2][5] = value;
+    }
+    else if (play == 6)
+    {
+        position[2][9] = value;
+    }
+    else if (play == 7)
+    {
+        position[4][1] = value;
+    }
+    else if (play == 8)
+    {
+        position[4][5] = value;
+    }
+    else if (play == 9)
+    {
+        position[4][9] = value;
+    }
 }
 
-bool esGanador(char areaPartida [3][3]){
-    if (turnoJugador%2!=0){
-         if (areaPartida[0][0]!='x'&&areaPartida[0][1]==areaPartida[0][0]&&areaPartida[0][2]==areaPartida[0][0]){
+bool isWinner()
+{
+    if (turn % 2 == 1)
+    {
+        if (position[0][1] == 'x' && position[0][5] == position[0][1] && position[0][9] == position[0][1])
+        {
             return true;
-        }else if (areaPartida[0][0]!='x'&&areaPartida[1][0]==areaPartida[0][0]&&areaPartida[2][0]==areaPartida[0][0]){
-            Ganador=true;
-        }else if (areaPartida[0][0]!='x'&&areaPartida[1][1]==areaPartida[0][0]&&areaPartida[2][2]==areaPartida[0][0]){
+        }
+        else if (position[2][1] == 'x' && position[2][5] == position[2][1] && position[2][9] == position[2][1])
+        {
             return true;
-        }else if (areaPartida[0][2]!='x'&&areaPartida[1][2]==areaPartida[0][2]&&areaPartida[2][2]==areaPartida[0][2]){
+        }
+        else if (position[4][1] == 'x' && position[4][5] == position[4][1] && position[4][9] == position[4][1])
+        {
             return true;
-        }else if (areaPartida[0][2]!='x'&&areaPartida[1][1]==areaPartida[0][2]&&areaPartida[2][0]==areaPartida[0][2]){
+        }
+        else if (position[0][1] == 'x' && position[2][1] == position[0][1] && position[4][1] == position[0][1])
+        {
             return true;
-        }else if (areaPartida[0][1]!='x'&&areaPartida[1][1]==areaPartida[0][1]&&areaPartida[2][1]==areaPartida[0][1]){
+        }
+        else if (position[0][5] == 'x' && position[2][5] == position[0][5] && position[4][5] == position[0][5])
+        {
             return true;
-        }else if (areaPartida[1][0]!='x'&&areaPartida[1][1]==areaPartida[1][0]&&areaPartida[1][2]==areaPartida[1][0]){
+        }
+        else if (position[0][9] == 'x' && position[2][9] == position[0][9] && position[4][9] == position[0][9])
+        {
             return true;
-        }else if (areaPartida[2][0]!='x'&&areaPartida[2][1]==areaPartida[2][0]&&areaPartida[2][2]==areaPartida[2][0]){
+        }
+        else if (position[0][1] == 'x' && position[2][5] == position[0][1] && position[4][9] == position[0][1])
+        {
             return true;
-        }else {
+        }
+        else if (position[4][1] == 'x' && position[2][5] == position[4][1] && position[0][9] == position[4][1])
+        {
+            return true;
+        }
+        else
+        {
             return false;
         }
-    }else 
-   
-        if (areaPartida[0][0]!='o'&&areaPartida[0][1]==areaPartida[0][0]&&areaPartida[0][2]==areaPartida[0][0]){
+    }
+    else
+    {
+        if (position[0][1] == 'o' && position[0][5] == position[0][1] && position[0][9] == position[0][1])
+        {
             return true;
-        }else if (areaPartida[0][0]!='o'&&areaPartida[1][0]==areaPartida[0][0]&&areaPartida[2][0]==areaPartida[0][0]){
+        }
+        else if (position[2][1] == 'o' && position[2][5] == position[2][1] && position[2][9] == position[2][1])
+        {
             return true;
-        }else if (areaPartida[0][0]!='o'&&areaPartida[1][1]==areaPartida[0][0]&&areaPartida[2][2]==areaPartida[0][0]){
+        }
+        else if (position[4][1] == 'o' && position[4][5] == position[4][1] && position[4][9] == position[4][1])
+        {
             return true;
-        }else if (areaPartida[0][2]!='o'&&areaPartida[1][2]==areaPartida[0][2]&&areaPartida[2][2]==areaPartida[0][2]){
+        }
+        else if (position[0][1] == 'o' && position[2][1] == position[0][1] && position[4][1] == position[0][1])
+        {
             return true;
-        }else if (areaPartida[0][2]!='o'&&areaPartida[1][1]==areaPartida[0][2]&&areaPartida[2][0]==areaPartida[0][2]){
+        }
+        else if (position[0][5] == 'o' && position[2][5] == position[0][5] && position[4][5] == position[0][5])
+        {
             return true;
-        }else if (areaPartida[0][1]!='o'&&areaPartida[1][1]==areaPartida[0][1]&&areaPartida[2][1]==areaPartida[0][1]){
+        }
+        else if (position[0][9] == 'o' && position[2][9] == position[0][9] && position[4][9] == position[0][9])
+        {
             return true;
-        }else if (areaPartida[1][0]!='o'&&areaPartida[1][1]==areaPartida[1][0]&&areaPartida[1][2]==areaPartida[1][0]){
+        }
+        else if (position[0][1] == 'o' && position[2][5] == position[0][1] && position[4][9] == position[0][1])
+        {
             return true;
-        }else if (areaPartida[2][0]!='o'&&areaPartida[2][1]==areaPartida[2][0]&&areaPartida[2][2]==areaPartida[2][0]){
+        }
+        else if (position[4][1] == 'o' && position[2][5] == position[4][1] && position[0][9] == position[4][1])
+        {
             return true;
-        }else {
+        }
+        else
+        {
             return false;
         }
-
+    }
 }
